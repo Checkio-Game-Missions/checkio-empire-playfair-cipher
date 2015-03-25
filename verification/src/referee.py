54e6f60f8a218c867c56c25434ad05dceb5a7e76
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-25 10:40:36 +0300

    refactors

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -8,8 +8,8 @@ from tests import TESTS
 
 class Referee(RefereeBase):
     TESTS = TESTS
-    EXECUTABLE_PATH = settings.EXECUTABLE_PATH
-    CURRENT_ENV = settings_env.CURRENT_ENV
+    ENVIRONMENTS = settings_env.ENVIRONMENTS
+
     ENV_COVERCODE = {
         "python_3": covercodes.py_unwrap_args,
         "python_2": covercodes.py_unwrap_args,
