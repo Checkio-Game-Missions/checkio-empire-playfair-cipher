f5a6ae83b117230069146a5bdb49175a0c7f7f74
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-05-27 16:09:53 +0300

    fix repr and initial

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -5,6 +5,20 @@ import settings_env
 from tests import TESTS
 
 
+def ext_str(data) -> str:
+    """
+    Extended str data conversion with quotes for strings.
+
+    :param data: data for conversion
+    :return: string representation
+    """
+    return '"{}"'.format(data) if isinstance(data, str) else str(data)
+
+
+def py_repr(test, _):
+    arguments = ", ".join(ext_str(d) for d in test["input"])
+    return "{}({})".format(test["function_name"], arguments)
+
 
 class Referee(RefereeBase):
     TESTS = TESTS
@@ -15,7 +29,5 @@ class Referee(RefereeBase):
         "python_2": covercodes.py_unwrap_args,
     }
     CALLED_REPRESENTATIONS = {
-        "python_3": representations.unwrap_arg_representation,
-        "python_2": representations.unwrap_arg_representation,
-        "javascript": representations.unwrap_arg_representation,
+        "python_3": py_repr,
     }
